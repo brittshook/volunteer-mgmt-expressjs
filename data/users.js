005fc2abcfc904a10816a640cd825616a81f6739
Britt Shook <brittanysshook@gmail.com>
2024-01-25 18:24:35 -0500

    add sample data

---
+++ data/users.js
@@ -0,0 +1,26 @@
+const users = [
+  {
+    first: "Stephanie",
+    last: "Flinn",
+    email: "stephanie@example.com",
+    address: "123 Billy Goat Rd., Seneca, SC, 29678",
+    password: "thisWouldBeHashedAndSalted",
+    role: ["owner", "admin"],
+  },
+  {
+    first: "Quinta",
+    last: "Thomas",
+    email: "quinta@example.com",
+    address: "1200 Octopus St., Charleston, SC, 29407",
+    password: "thisWouldBeHashedAndSalted2",
+    role: ["admin"],
+  },
+  {
+    first: "Tim",
+    last: "Turner",
+    email: "tim@example.com",
+    address: "13 Main St., Irmo, SC, 29063",
+    password: "thisWouldBeHashedAndSalted2",
+    role: ["volunteer"],
+  },
+];
---
+++ data/volunteerProgram.js
@@ -0,0 +1,8 @@
+const programs = [
+  "Skill up for the Buildup",
+  "Organize the Rise",
+  "Rooted",
+  "Scale to Sale",
+  "New Life",
+  "Hands that Heal",
+];
