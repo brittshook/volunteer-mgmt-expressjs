07cc0405e29a4413789f20e6ca0f02ad9db3c688
Britt Shook <brittanysshook@gmail.com>
2024-01-25 21:35:44 -0500

    reorg programs to include id

---
+++ data/programs.js
@@ -0,0 +1,8 @@
+const programs = [
+  { id: 1, name: "Skill up for the Buildup" },
+  { id: 2, name: "Organize the Rise" },
+  { id: 3, name: "Rooted" },
+  { id: 4, name: "Scale to Sale" },
+  { id: 5, name: "New Life" },
+  { id: 6, name: "Hands that Heal" },
+];
