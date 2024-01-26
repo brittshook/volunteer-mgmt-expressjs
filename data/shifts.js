c407af9b7ea56a4ad037581160b2735e1b391848
Britt Shook <brittanysshook@gmail.com>
2024-01-26 00:01:39 -0500

    add sample shift

--- data/shifts.js
+++ data/shifts.js
@@ -1,3 +1,13 @@
-const shifts = [];
+const shifts = [
+  {
+    id: 1,
+    name: "Tree Planting Workshop",
+    program: "Rooted",
+    startTime: "2024-01-25T12:30:00.000",
+    endTime: "2024-01-25T14:30:00.000",
+    location: "McCormick, SC",
+    volunteerLimit: 20,
+  },
+];
 
 module.exports = shifts;
