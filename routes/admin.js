c451613717390a2097d06b24928bf6a309e167b0
Britt Shook <brittanysshook@gmail.com>
2024-01-25 21:33:39 -0500

    add admin approve user route

---
+++ routes/admin.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const users = require("../data/users");
+const error = require("../utils/error");
+
+router.patch("/approve/:userId/?", (req, res, next) => {
+  const userId = req.params.userId;
+  const status = req.body.status;
+
+  const user = users.find((user) => user.id == userId);
+  if (userId && status) {
+    if (user) {
+      user.status = status;
+    } else {
+      next(error(404, "User not found"));
+    }
+  } else {
+    next(error(400, "Insufficient data"));
+  }
+});
