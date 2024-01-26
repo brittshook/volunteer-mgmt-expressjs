8cd96f3d9935d2e96392e11b177ac11a22c587cf
Britt Shook <brittanysshook@gmail.com>
2024-01-25 21:37:13 -0500

    add uuid, status

--- data/users.js
+++ data/users.js
@@ -1,26 +1,29 @@
 const users = [
   {
+    id: "tGCNGcHV",
     first: "Stephanie",
     last: "Flinn",
     email: "stephanie@example.com",
     address: "123 Billy Goat Rd., Seneca, SC, 29678",
-    password: "thisWouldBeHashedAndSalted",
     role: ["owner", "admin"],
+    status: "approved",
   },
   {
+    id: "G5mCv43P",
     first: "Quinta",
     last: "Thomas",
     email: "quinta@example.com",
     address: "1200 Octopus St., Charleston, SC, 29407",
-    password: "thisWouldBeHashedAndSalted2",
     role: ["admin"],
+    status: "approved",
   },
   {
+    id: "YmJijswK",
     first: "Tim",
     last: "Turner",
     email: "tim@example.com",
     address: "13 Main St., Irmo, SC, 29063",
-    password: "thisWouldBeHashedAndSalted2",
     role: ["volunteer"],
+    status: "approved",
   },
 ];
